050eb60d31533e1662d58ad172352038bb07ad41
sora <sora@haeena.net>
2014-05-31 17:47:29 +0900

    indent

--- ethpipe.c
+++ ethpipe.c
@@ -42,63 +42,63 @@ int counter_tmp, lap1_tmp, lap2_tmp;
 static ssize_t counter_show( struct file *file, char *buf, size_t count,
 				loff_t *ppos )
 {
-  if (debug)
-    printk("%s\n", __func__);
-
-  sprintf(buf, "%llX\n", *reg_counter);
-  return count;
+	if (debug)
+		printk("%s\n", __func__);
+	
+	sprintf(buf, "%llX\n", *reg_counter);
+	return count;
 }
 
 static ssize_t lap1_show( struct file *file, char *buf, size_t count,
 				loff_t *ppos )
 {
-  if (debug)
-    printk("%s\n", __func__);
-
-  return sprintf(buf, "%llX\n", *reg_lap1);
+	if (debug)
+		printk("%s\n", __func__);
+	
+	return sprintf(buf, "%llX\n", *reg_lap1);
 }
 
 static ssize_t lap1_store(struct file *file, const char __user *buf,
-        size_t count, loff_t *ppos)
+				size_t count, loff_t *ppos)
 {
-  if (debug)
-    printk("%s\n", __func__);
-
-  sscanf(buf, "%llX", reg_lap1);
-  return count;
+	if (debug)
+		printk("%s\n", __func__);
+	
+	sscanf(buf, "%llX", reg_lap1);
+	return count;
 }
 
 static ssize_t lap2_show( struct file *file, char *buf, size_t count,
 				loff_t *ppos )
 {
-  if (debug)
-    printk("%s\n", __func__);
-
-  return sprintf(buf, "%llX\n", *reg_lap2);
+	if (debug)
+		printk("%s\n", __func__);
+	
+	return sprintf(buf, "%llX\n", *reg_lap2);
 }
 
 static ssize_t lap2_store(struct file *file, const char __user *buf,
-        size_t count, loff_t *ppos)
+				size_t count, loff_t *ppos)
 {
-  if (debug)
-    printk("%s\n", __func__);
+	if (debug)
+		printk("%s\n", __func__);
 
-  sscanf(buf, "%llX", reg_lap2);
-  return count;
+	sscanf(buf, "%llX", reg_lap2);
+	return count;
 }
 
 static const struct file_operations proc_counter_fops = {
-  .read = counter_show,
+	.read = counter_show,
 };
 
 static const struct file_operations proc_lap1_fops = {
-  .read = lap1_show,
-  .write = lap1_store,
+	.read = lap1_show,
+	.write = lap1_store,
 };
 
 static const struct file_operations proc_lap2_fops = {
-  .read = lap2_show,
-  .write = lap2_store,
+	.read = lap2_show,
+	.write = lap2_store,
 };
 
 /**
@@ -107,8 +107,8 @@ static const struct file_operations proc_lap2_fops = {
  **/
 static int ep_open(struct inode *inode, struct file *file)
 {
-  if (debug)
-    printk("%s\n", __func__);
+	if (debug)
+		printk("%s\n", __func__);
 
 	return 0;
 }
@@ -123,8 +123,8 @@ static ssize_t ep_write(struct file *file, const char __user *buf,
 	unsigned int copy_len = 0;
 	static unsigned char pkt[EP_HDR_LEN+MAX_PKT_LEN] = {0};
 
-  if (debug)
-    printk("%s\n", __func__);
+	if (debug)
+		printk("%s\n", __func__);
 
 	if (count > (MAX_BUF_LEN - buf_pos)) {
 		copy_len = MAX_BUF_LEN - buf_pos;
@@ -140,13 +140,13 @@ static ssize_t ep_write(struct file *file, const char __user *buf,
 	*ppos += copy_len;
 	buf_pos += copy_len;
 
-  if (debug) {
-    printk( KERN_INFO "buf_pos = %d\n", buf_pos );
-
-    printk( "DEBUG: pkt = %02x%02x%02x%02x%02x%02x %02x%02x%02x%02x%02x%02x\n",
-      pkt[ 0], pkt[ 1], pkt[ 2], pkt[ 3], pkt[ 4], pkt[ 5], pkt[ 6], pkt[ 7],
-      pkt[ 8], pkt[ 9], pkt[10], pkt[11] );
-  }
+	if (debug) {
+		printk( KERN_INFO "buf_pos = %d\n", buf_pos );
+	
+		printk( "DEBUG: pkt = %02x%02x%02x%02x%02x%02x %02x%02x%02x%02x%02x%02x\n",
+			pkt[ 0], pkt[ 1], pkt[ 2], pkt[ 3], pkt[ 4], pkt[ 5], pkt[ 6], pkt[ 7],
+			pkt[ 8], pkt[ 9], pkt[10], pkt[11] );
+	}
 
 	return count;
 }
@@ -157,8 +157,8 @@ static ssize_t ep_write(struct file *file, const char __user *buf,
  **/
 static int ep_release(struct inode *inode, struct file *file)
 {
-  if (debug)
-    printk("%s\n", __func__);
+	if (debug)
+		printk("%s\n", __func__);
 
 	return 0;
 }
@@ -184,13 +184,13 @@ static int ep_init_one(void)
 {
 	static char devname[16];
 	static int board_idx = -1;
-  struct proc_dir_entry *pe_counter, *pe_lap1, *pe_lap2;
+	struct proc_dir_entry *pe_counter, *pe_lap1, *pe_lap2;
 	int ret;
 
-  /* tmp */
-  reg_counter = &counter_data;
-  reg_lap1 = &lap1_data;
-  reg_lap2 = &lap2_data;
+	/* tmp */
+	reg_counter = &counter_data;
+	reg_lap1 = &lap1_data;
+	reg_lap2 = &lap2_data;
 
 	++board_idx;
 	printk( KERN_INFO "board_idx: %d\n", board_idx );
@@ -204,41 +204,41 @@ static int ep_init_one(void)
 		return ret;
 	}
 
-  /* register ethpipe procfs entries */
-  ep_proc_root = proc_mkdir(EP_PROC_DIR, NULL);    // proc root dir
-  if (!ep_proc_root) {
-    pr_warn("cannot create /proc/%s\n", EP_PROC_DIR);
-    return -ENODEV;
-  }
-  pe_counter = proc_create("counter", 0666, ep_proc_root, &proc_counter_fops);    // counter
-  if (pe_counter == NULL) {
-    pr_err("cannot create %s procfs entry\n", "counter");
-    ret = -EINVAL;
-    goto remove_counter;
-  }
-  pe_lap1 = proc_create("lap1", 0666, ep_proc_root, &proc_lap1_fops);    // lap1
-  if (pe_lap1 == NULL) {
-    pr_err("cannot create %s procfs entry\n", "lap1");
-    ret = -EINVAL;
-    goto remove_lap1;
-  }
-  pe_lap2 = proc_create("lap2", 0666, ep_proc_root, &proc_lap2_fops);    // lap2
-  if (pe_lap2 == NULL) {
-    pr_err("cannot create %s procfs entry\n", "lap2");
-    ret = -EINVAL;
-    goto remove_lap2;
-  }
+	/* register ethpipe procfs entries */
+	ep_proc_root = proc_mkdir(EP_PROC_DIR, NULL);    // proc root dir
+	if (!ep_proc_root) {
+		pr_warn("cannot create /proc/%s\n", EP_PROC_DIR);
+		return -ENODEV;
+	}
+	pe_counter = proc_create("counter", 0666, ep_proc_root, &proc_counter_fops);    // counter
+	if (pe_counter == NULL) {
+		pr_err("cannot create %s procfs entry\n", "counter");
+		ret = -EINVAL;
+		goto remove_counter;
+	}
+	pe_lap1 = proc_create("lap1", 0666, ep_proc_root, &proc_lap1_fops);    // lap1
+	if (pe_lap1 == NULL) {
+		pr_err("cannot create %s procfs entry\n", "lap1");
+		ret = -EINVAL;
+		goto remove_lap1;
+	}
+	pe_lap2 = proc_create("lap2", 0666, ep_proc_root, &proc_lap2_fops);    // lap2
+	if (pe_lap2 == NULL) {
+		pr_err("cannot create %s procfs entry\n", "lap2");
+		ret = -EINVAL;
+		goto remove_lap2;
+	}
 
 	return 0;
 
 remove_lap2:
-  remove_proc_entry("lap2", ep_proc_root);
+	remove_proc_entry("lap2", ep_proc_root);
 remove_lap1:
-  remove_proc_entry("lap1", ep_proc_root);
+	remove_proc_entry("lap1", ep_proc_root);
 remove_counter:
-  remove_proc_entry("counter", ep_proc_root);
-  remove_proc_entry(EP_PROC_DIR, NULL);
-  return ret;
+	remove_proc_entry("counter", ep_proc_root);
+	remove_proc_entry(EP_PROC_DIR, NULL);
+	return ret;
 }
 
 /**
@@ -260,10 +260,10 @@ static void __exit ep_cleanup(void)
 	printk("%s\n", __func__);
 	misc_deregister(&ep_misc_device);
 
-  remove_proc_entry("lap2", ep_proc_root);
-  remove_proc_entry("lap1", ep_proc_root);
-  remove_proc_entry("counter", ep_proc_root);
-  remove_proc_entry(EP_PROC_DIR, NULL);
+	remove_proc_entry("lap2", ep_proc_root);
+	remove_proc_entry("lap1", ep_proc_root);
+	remove_proc_entry("counter", ep_proc_root);
+	remove_proc_entry(EP_PROC_DIR, NULL);
 }
 
 module_init(ep_init);
